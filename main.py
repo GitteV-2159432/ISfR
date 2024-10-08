7ba526852dd53707b8bda03ce195c440802ef6bb
VinzR-2159109 <vinz.roosen@student.uhasselt.be>
2024-10-08 11:38:30 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sapien
 from sapien import Pose
 import numpy as np
+import environment
+
 
 def main():
     scene = sapien.Scene()
@@ -10,13 +12,12 @@ def main():
     viewer.set_camera_xyz(x=-12, y=0, z=15)
     viewer.set_camera_rpy(r=0, p=-np.arctan2(2, 2), y=0)
     viewer.window.set_camera_parameters(near=0.05, far=100, fovy=1)
-
+    
+    #environment.create_wall_grid(scene=scene, grid_size=4, spacing=4)
 
     while not viewer.closed:
         scene.step()
         scene.update_render()
-
-        # lidar_sensor.visualize()
 
 if __name__ == "__main__":
     main()

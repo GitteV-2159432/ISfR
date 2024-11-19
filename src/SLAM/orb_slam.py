ecd8d9b223d69340a9ee9d1d5b6be8a8fd4694ef
Lars Gielen <larsgielen2003@gmail.com>
2024-11-19 11:45:31 +0100

    Refactor to class

--- src/SLAM/orb_slam.py
+++ src/SLAM/orb_slam.py
@@ -1,32 +1,59 @@
 import cv2 as cv
 import numpy as np
 
-def init_orb():
+class OrbSlam():
+    def __init__(self, camera_matrix = np.eye(3), dist_coeffs = np.zeros(5)):
+        self.orb_detector = _init_orb()
+        self.flann_matcher = _init_flann()
+        self.camera_matrix = camera_matrix
+        self.dist_coeffs = dist_coeffs
+        self.prev_kp, self.prev_des = None
+
+    def run(self, image):
+        current_kp, current_des = _detect_orb(image, self.orb_detector)
+        transformation_matrix = np.eye(4)
+        points_relative_3d = []
+
+        # When first time loop is running -> set prev kp, des and skip to next iteration
+        if (self.prev_kp is not None or self.prev_des is not None):
+            matches = _match_descriptors(self.flann_matcher, self.prev_des, current_des)
+            prev_points, current_points = _get_points_from_matches(self.prev_kp, current_kp, matches)
+
+            transformation_matrix, _ = _calculate_transformation_matrix(prev_points, current_points, self.camera_matrix, self.dist_coeffs)
+            points_relative_3d = _calculate_3D_points(self.prev_kp, current_kp, transformation_matrix, self.camera_matrix)
+
+        # Set prev to current for next loop
+        self.prev_kp = current_kp
+        self.prev_des = current_des
+
+        return transformation_matrix, points_relative_3d
+
+def _init_orb():
     orb_detector = cv.ORB.create(nfeatures=50)
     return orb_detector
 
-def detect_orb(img, orb_detector):
+def _detect_orb(img, orb_detector):
     gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     kp = orb_detector.detect(gray_img, None)
     kp, des = orb_detector.compute(gray_img, kp)
 
     return kp, des
 
-def init_flann():
+def _init_flann():
     flann_matcher = cv.FlannBasedMatcher.create()
     return flann_matcher
 
-def match_des(flann_matcher, prev_des, current_des):
+def _match_descriptors(flann_matcher, prev_des, current_des, threshold: float = 30):
     matches = flann_matcher.knnMatch(prev_des, current_des)
-    matches = [m for m in matches if m.distance < 30] # 30 is a threshold value to filter out really bad matches
+    matches = [m for m in matches if m.distance < threshold] # 30 is a threshold value to filter out really bad matches
     return matches
 
-def get_points_from_matches(prev_kp, current_kp, matches):
+def _get_points_from_matches(prev_kp, current_kp, matches):
     prev_points = [prev_kp[m.queryIdx].pt for m in matches]
     current_points = [current_kp[m.trainIdx].pt for m in matches]
     return prev_points, current_points
 
-def calculate_3D_points(prev_points, current_points, transformation_matrix, camera_matrix=np.eye(3)):
+def _calculate_3D_points(prev_points, current_points, transformation_matrix, camera_matrix=np.eye(3)):
     projMatr1 = np.hstack((camera_matrix, np.zeros((3, 1))))  # Identiteitsmatrix voor vorige frame
     projMatr2 = np.hstack((camera_matrix @ transformation_matrix[:3, :3], camera_matrix @ transformation_matrix[:3, 3].reshape(-1, 1)))
 
@@ -35,13 +62,7 @@ def calculate_3D_points(prev_points, current_points, transformation_matrix, came
 
     return points_3D
 
-def calculate_camera_position(prev_position, transformation_matrix):
-    prev_position_hom = cv.convertPointsToHomogeneous(prev_position)
-    current_position_hom = prev_position_hom @ transformation_matrix.T
-    current_position = cv.convertPointsFromHomogeneous(current_position_hom)
-    return current_position
-
-def calculate_transformation_matrix(prev_points, current_points, camera_matrix=np.eye(3), dist_coeffs=np.zeros(5)):
+def _calculate_transformation_matrix(prev_points, current_points, camera_matrix=np.eye(3), dist_coeffs=np.zeros(5)):
     E, _ = cv.findEssentialMat(prev_points, current_points, camera_matrix)
     _, R, t, _ = cv.recoverPose(E, prev_points, current_points, camera_matrix)
 
@@ -49,84 +70,4 @@ def calculate_transformation_matrix(prev_points, current_points, camera_matrix=n
     transformation_matrix[:3, :3] = R
     transformation_matrix[:3, 3] = t.flatten()
 
-    return transformation_matrix, R, t
-
-# Graph algo -> not used, Lars made a better version
-def add_new_to_global_data(flann_matcher, points_3D, current_des, global_points_3D, global_des, global_weights):
-    matches = flann_matcher.knnMatch(points_3D, global_points_3D)
-    matches = [m for m in matches if m.distance < 20] # 20 is a threshold value to filter out really bad matches
-    
-    points_3D_matched = [points_3D[m.queryIdx].pt for m in matches]
-    global_points_3D_matched = [global_points_3D[m.trainIdx].pt for m in matches]
-
-    for i in range(len(points_3D)):
-        point = points_3D[i]
-        if point not in points_3D_matched: # Als het punt niet gematched is -> staat nog niet in de global list
-            global_points_3D.append(point)
-            global_des.append(current_des[i])
-            global_weights.append(10)            
-        else: # Anders staat die wel al in de global list -> weight verhogen
-            global_index = global_points_3D.index(global_points_3D_matched[points_3D_matched.index(point)])
-            global_weights[global_index] += 1
-
-    for i in range(global_points_3D):
-        point = global_points_3D[i]
-        if point not in global_points_3D_matched and global_weights[i] < 20: # Punten met een weight groter dan 20 laten we (permanent) staan
-            global_weights[i] -= 1
-
-        if global_weights[i] <= 0:
-            global_points_3D.pop(i)
-            global_des.pop(i)
-            global_weights.pop(i)
-
-def visualize(img, kp):
-    img = cv.drawKeypoints(img, kp, None)
-    cv.imshow("camera", img)
-    cv.waitKey(1)
-
-def main():
-    orb_detector = init_orb()
-    flann_matcher = init_flann()
-
-    # global_points_3D = []
-    # global_des = []
-    # global_weights = []
-
-    prev_kp, prev_des = None
-    camera_position = [.0, .0, .0]
-
-    # TODO change to camera on robot in simulation!
-    cap = cv.VideoCapture(0)
-
-    while (True):
-        _, img = cap.read()
-
-        current_kp, current_des = detect_orb(img, orb_detector)
-        
-        # When first time loop is running -> set prev kp, des and skip to next iteration
-        if (prev_kp is None or prev_des is None):
-            # Set current to prev for next loop
-            prev_kp = current_kp
-            prev_des = current_des
-            continue
-
-        matches = match_des(flann_matcher, prev_des, current_des)
-
-        prev_points, current_points = get_points_from_matches(prev_kp, current_kp, matches)
-
-        transformation_matrix, _ = calculate_transformation_matrix(prev_points, current_points)
-        camera_position = calculate_camera_position(camera_position, transformation_matrix)
-
-        points_3D = calculate_3D_points(prev_points, current_points, transformation_matrix)
-
-        # TODO send points_3D to Lars Graph algo
-        # add_new_to_global_data(flann_matcher, points_3D, current_des, global_points_3D, global_des, global_weights)
-
-        visualize(img, current_kp)
-
-        # Set current to prev for next loop
-        prev_kp = current_kp
-        prev_des = current_des
-
-if __name__ == "__main__":
-    main()
+    return transformation_matrix, R, t   
